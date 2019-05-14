6357120970d1294aed1225ece28cd8000e72f669
Ben <ben@zfben.com>
2019-05-14 14:01:59 +0800

    update flow version

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'lib/index.js',
+      format: 'cjs'
+    },
+    {
+      file: 'lib/index.es.js',
+      format: 'es'
+    }
+  ],
+  plugins: [
+    typescript({
+      tsconfigOverride: {
+        compilerOptions: {
+          module: 'esnext'
+        }
+      }
+    })
+  ]
+};
